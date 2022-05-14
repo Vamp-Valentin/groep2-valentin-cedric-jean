e96808d0b0e3b20694c4b3ab9c7fd83f75056b9b
Cédric Jonkers <Jonkerscedric@gmail.com>
2022-05-14 23:37:58 +0200

    t

--- Intro-mobile-project/exam_app/lib/screens/admins/location/location_on_map.dart
+++ Intro-mobile-project/exam_app/lib/screens/admins/location/location_on_map.dart
@@ -1,5 +1,4 @@
 import 'package:exam_app/screens/students/home/home.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:flutter/material.dart';
 
 class LocationOnMap extends StatefulWidget {
@@ -13,15 +12,6 @@ class LocationOnMap extends StatefulWidget {
 class _LocationOnMapState extends State<LocationOnMap> {
   
   @override
-  
-   MapController controller = MapController(
-                            initMapWithUserPosition: false,
-                            initPosition: GeoPoint(latitude: 47.4358055, longitude: 8.4737324),
-                            areaLimit: BoundingBox( east: 10.4922941, north: 47.8084648, south: 45.817995, west: 5.9559113,),
-                       );
-    
-
-
   Widget build(BuildContext context) {
     return Container(child: Text("hey"),);
   }
