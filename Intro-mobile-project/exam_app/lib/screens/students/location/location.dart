a8027e000c979341703e9ffc91b32b21a906a9c0
Cédric Jonkers <Jonkerscedric@gmail.com>
2022-05-14 22:27:44 +0200

    loc

--- Intro-mobile-project/exam_app/lib/screens/students/location/location.dart
+++ Intro-mobile-project/exam_app/lib/screens/students/location/location.dart
@@ -8,91 +8,8 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:geolocator/geolocator.dart';
 
-// class LocationWidget extends StatefulWidget {
-//   //final getLocation;
-//   final String? stu;
-//   const LocationWidget({Key? key, this.stu}) : super(key: key);
-  
-
-//   @override
-//   State<LocationWidget> createState() => _LocationWidgetState();
-// }
-
-// class _LocationWidgetState extends State<LocationWidget> {
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     getLocation();
-//   }
-
-//   Position? position;
-//   final AuthService _auth = AuthService();
-
-//   getLocation() async {
-//     bool serviceEnabled;
-//     LocationPermission permission;
-//     String latitude;
-//     String longlatitude;
-
-//     serviceEnabled = await Geolocator.isLocationServiceEnabled();
-//     if (!serviceEnabled) {
-//       Fluttertoast.showToast(
-//           msg: "Location services are disabled.",
-//           toastLength: Toast.LENGTH_SHORT,
-//           gravity: ToastGravity.CENTER,
-//           timeInSecForIosWeb: 1,
-//           backgroundColor: Colors.red,
-//           textColor: Colors.white,
-//           fontSize: 16.0);
-//     }
-
-//     permission = await Geolocator.checkPermission();
-//     if (permission == LocationPermission.denied) {
-//       permission = await Geolocator.requestPermission();
-//       if (permission == LocationPermission.denied) {
-//         Fluttertoast.showToast(
-//             msg: "Location permissions are denied",
-//             toastLength: Toast.LENGTH_SHORT,
-//             gravity: ToastGravity.CENTER,
-//             timeInSecForIosWeb: 1,
-//             backgroundColor: Colors.red,
-//             textColor: Colors.white,
-//             fontSize: 16.0);
-//       }
-//     }
-
-//     if (permission == LocationPermission.deniedForever) {
-//       Fluttertoast.showToast(
-//           msg:
-//               'Location permissions are permanently denied, we cannot request permissions.',
-//           toastLength: Toast.LENGTH_SHORT,
-//           gravity: ToastGravity.CENTER,
-//           timeInSecForIosWeb: 1,
-//           backgroundColor: Colors.red,
-//           textColor: Colors.white,
-//           fontSize: 16.0);
-//     }
-
-//     Position currentPosition = await Geolocator.getCurrentPosition();
-//     setState(() {
-//       position = currentPosition;
-
-//       latitude = currentPosition.latitude.toString();
-//       longlatitude = currentPosition.longitude.toString();
-//       print(latitude);
-//       print(longlatitude);
-//       print(StudentDropdown().student.toString());
-//       DatabaseService(uid: "S183457").updateLatitudeAndLonglatitude(currentPosition.latitude.toString(), currentPosition.longitude.toString());
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container();
-//   }
-// }
 class LocationFunc{
+
     getLocation(String? student) async {
     Position? position;
     bool serviceEnabled;
@@ -142,22 +59,10 @@ class LocationFunc{
     Position currentPosition = await Geolocator.getCurrentPosition();
 
       position = currentPosition;
-
       latitude = currentPosition.latitude.toString();
       longlatitude = currentPosition.longitude.toString();
       print(latitude);
       print(longlatitude);
-      print(StudentDropdown().student.toString());
       DatabaseService(uid: student).updateLatitudeAndLonglatitude(currentPosition.latitude.toString(), currentPosition.longitude.toString());
-    // setState(() {
-    //   position = currentPosition;
-
-    //   latitude = currentPosition.latitude.toString();
-    //   longlatitude = currentPosition.longitude.toString();
-    //   print(latitude);
-    //   print(longlatitude);
-    //   print(StudentDropdown().student.toString());
-    //   DatabaseService(uid: "S183457").updateLatitudeAndLonglatitude(currentPosition.latitude.toString(), currentPosition.longitude.toString());
-    // });
   }
 }
