4f2df44ca68cea0ef0ac670d44b875dcccb3e8bd
Cédric Jonkers <Jonkerscedric@gmail.com>
2022-05-15 14:52:25 +0200

    loc_widget

--- Intro-mobile-project/exam_app/lib/screens/admins/location/location_on_map.dart
+++ Intro-mobile-project/exam_app/lib/screens/admins/location/location_on_map.dart
@@ -7,43 +7,37 @@ import 'package:provider/provider.dart';
 
 import '../../../models/my_exam.dart';
 
-
 class LocationOnMap extends StatefulWidget {
   LocationOnMap({Key? key}) : super(key: key);
 
   @override
   State<LocationOnMap> createState() => _LocationOnMapState();
-   
 }
 
 class _LocationOnMapState extends State<LocationOnMap> {
-  
-Widget build(BuildContext context) {
-  
-  return new FlutterMap(
-    options: new MapOptions(
-      center: new LatLng(43, -0.09),
-      zoom: 13.0,
-    ),
-    layers: [
-      new TileLayerOptions(
-        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-        subdomains: ['a', 'b', 'c']
+  Widget build(BuildContext context) {
+    return new FlutterMap(
+      options: new MapOptions(
+        center: new LatLng(51.3012, 4.4891),
+        zoom: 15.0,
       ),
-      new MarkerLayerOptions(
-        markers: [
-          new Marker(
-            width: 40.0,
-            height: 40.0,
-            point: new LatLng(43, -0.09),
-            builder: (ctx) =>
-            new Container(
-              child: new FlutterLogo(),
+      layers: [
+        new TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c']),
+        new MarkerLayerOptions(
+          markers: [
+            new Marker(
+              width: 40.0,
+              height: 40.0,
+              point: new LatLng(51.3012, 4.4891),
+              builder: (ctx) => new Container(
+                child: new Icon(Icons.location_on, color: Colors.red,)
+              ),
             ),
-          ),
-        ],
-      ),
-    ],
-  );
-}
+          ],
+        ),
+      ],
+    );
+  }
 }
