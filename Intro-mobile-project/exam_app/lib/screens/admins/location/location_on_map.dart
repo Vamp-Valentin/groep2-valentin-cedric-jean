3dee7128665826793786cc03b527935b60b9a5c6
Jean Bacquaert <Jean.bacquaert@gmail.com>
2022-05-15 14:18:22 +0200

    map tonen

--- Intro-mobile-project/exam_app/lib/screens/admins/location/location_on_map.dart
+++ Intro-mobile-project/exam_app/lib/screens/admins/location/location_on_map.dart
@@ -1,5 +1,12 @@
+import 'package:exam_app/models/my_student.dart';
 import 'package:exam_app/screens/students/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
+import 'package:provider/provider.dart';
+
+import '../../../models/my_exam.dart';
+
 
 class LocationOnMap extends StatefulWidget {
   LocationOnMap({Key? key}) : super(key: key);
@@ -11,8 +18,32 @@ class LocationOnMap extends StatefulWidget {
 
 class _LocationOnMapState extends State<LocationOnMap> {
   
-  @override
-  Widget build(BuildContext context) {
-    return Container(child: Text("hey"),);
-  }
+Widget build(BuildContext context) {
+  
+  return new FlutterMap(
+    options: new MapOptions(
+      center: new LatLng(43, -0.09),
+      zoom: 13.0,
+    ),
+    layers: [
+      new TileLayerOptions(
+        urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        subdomains: ['a', 'b', 'c']
+      ),
+      new MarkerLayerOptions(
+        markers: [
+          new Marker(
+            width: 40.0,
+            height: 40.0,
+            point: new LatLng(43, -0.09),
+            builder: (ctx) =>
+            new Container(
+              child: new FlutterLogo(),
+            ),
+          ),
+        ],
+      ),
+    ],
+  );
+}
 }
